a60b313fef67f2890b8144fe1080852c47c4e975
Ronaksavaliya0701 <savaliyaronak0701@gmail.com>
2023-06-13 10:49:22 +0530

    useRef

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cart from '../../counter-master/src/component/Cart/Cart'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Cart/>
+  </React.StrictMode>,
+)
